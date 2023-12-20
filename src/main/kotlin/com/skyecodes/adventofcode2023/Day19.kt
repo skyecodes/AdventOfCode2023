0d1dfb51ce5aeec7d7eea97de56857efa2995eb7
skyecodes <skyecodes@proton.me>
2023-12-20 18:56:44 +0100

    feat: day 19 (part 2)

--- src/main/kotlin/com/skyecodes/adventofcode2023/Day19.kt
+++ src/main/kotlin/com/skyecodes/adventofcode2023/Day19.kt
@@ -1,5 +1,8 @@
 package com.skyecodes.adventofcode2023
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     Day19.test()
     Day19.run()
@@ -39,35 +42,76 @@ private object Day19 : NormalDay<WorkflowRating, Int, Long>() {
 
     override fun part1(input: WorkflowRating): Int = input.ratings.filter { runWorkflow("in", input.workflows, it) == 'A' }.sumOf { it.values.sum() }
 
-    private tailrec fun runWorkflow(name: String, workflows: Map<String, Workflow>, rating: Map<Char, Int>): Char {
+    private tailrec fun runWorkflow(name: String, workflows: Map<String, Workflow>, rating: Rating): Char {
         val result = workflows[name]!!.firstNotNullOf { it.checkRule(rating) }
         return if (result in finalResults) result[0] else runWorkflow(result, workflows, rating)
     }
 
-    override fun part2(input: WorkflowRating): Long {
-        /*val range = 1..4000
-        return range.sumOf { x -> range.sumOf { m -> range.sumOf { a -> range.sumOf { s ->
-            val rating = mapOf('x' to x, 'm' to m, 'a' to a, 's' to s)
-            if (runWorkflow("in", input.workflows, rating) == 'A') (x + m + a + s).toLong() else 0L
-        } } } }*/
-        return 167409079868000L
-    }
+    override fun part2(input: WorkflowRating): Long = runWorkflowRange("in", input.workflows, mapOf('x' to (1 to 4000), 'm' to (1 to 4000), 'a' to (1 to 4000), 's' to (1 to 4000)))
 
+    private fun runWorkflowRange(name: String, workflows: Map<String, Workflow>, rating: RangeRating, result: Long = 0L): Long {
+        var res = result
+        val workflow = workflows[name]!!
+        var curRating = rating
+        for (it in workflow) {
+            val (acceptedRating, rejectedRating) = it.splitRange(curRating)
+            if (it.result == "A") {
+                res += acceptedRating.values.fold(1L) { acc, intRange -> acc * (intRange.second - intRange.first + 1) }
+            } else if (it.result != "R") {
+                res += runWorkflowRange(it.result, workflows, acceptedRating, result)
+            }
+            curRating = rejectedRating
+        }
+        return res
+    }
 }
 
 private typealias Workflow = List<Rule>
 
 private sealed class Rule(val result: String) {
-    abstract fun checkRule(rating: Map<Char, Int>): String?
+    abstract fun checkRule(rating: Rating): String?
+    abstract fun splitRange(rating: RangeRating): Pair<RangeRating, RangeRating>
 }
 
-private class IfRule(val part: Char, opSign: Char, val value: Int, result: String) : Rule(result) {
+private class IfRule(val part: Char, val opSign: Char, val value: Int, result: String) : Rule(result) {
     private val op: (Int, Int) -> Boolean = if (opSign == '>') { a, b -> a > b } else { a, b -> a < b }
-    override fun checkRule(rating: Map<Char, Int>): String? = if (op(rating[part]!!, value)) result else null
+
+    override fun checkRule(rating: Rating): String? = if (op(rating[part]!!, value)) result else null
+
+    override fun splitRange(rating: RangeRating): Pair<RangeRating, RangeRating> {
+        val ratingToSplit = rating[part]!!
+        val rangeAccepted: Pair<Int, Int>
+        val rangeRejected: Pair<Int, Int>
+        if (opSign == '>') {
+            rangeAccepted = max(ratingToSplit.first, value) + 1 to ratingToSplit.second
+            rangeRejected = ratingToSplit.first to min(ratingToSplit.second, value)
+        } else {
+            rangeAccepted = ratingToSplit.first to min(ratingToSplit.second, value) - 1
+            rangeRejected = min(ratingToSplit.second, value) to ratingToSplit.second
+        }
+        val ratingAccepted = mutableMapOf<Char, Pair<Int, Int>>()
+        val ratingRejected = mutableMapOf<Char, Pair<Int, Int>>()
+        rating.forEach { (k, v) ->
+            if (k == part) {
+                ratingAccepted[k] = rangeAccepted
+                ratingRejected[k] = rangeRejected
+            } else {
+                ratingAccepted[k] = v
+                ratingRejected[k] = v
+            }
+        }
+        return ratingAccepted to ratingRejected
+    }
 }
 
 private class ElseRule(result: String) : Rule(result) {
-    override fun checkRule(rating: Map<Char, Int>): String = result
+    override fun checkRule(rating: Rating): String = result
+
+    override fun splitRange(rating: RangeRating): Pair<RangeRating, RangeRating> = rating to emptyMap()
 }
 
-private data class WorkflowRating(val workflows: Map<String, Workflow>, val ratings: List<Map<Char, Int>>)
+private data class WorkflowRating(val workflows: Map<String, Workflow>, val ratings: List<Rating>)
+
+private typealias Rating = Map<Char, Int>
+
+private typealias RangeRating = Map<Char, Pair<Int, Int>>
